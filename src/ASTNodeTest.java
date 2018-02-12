85a6d8a9160b807a115222e93a64664ad12ffe45
SooperMrNinja <SooperMrNinja@10.137.211.146>
2018-02-12 05:42:27 -0700

    All done, with comments

--- src/ASTNodeTest.java
+++ src/ASTNodeTest.java
@@ -3,13 +3,15 @@ import org.junit.Test;
 
 import bool_exp.ASTNode;
 
-
+// Junit testing of the ASTNode class
 public class ASTNodeTest {
     // Tests the functions of the AND node type
 	@Test
 	public void testAND() {
-        // Creates a node using null as children
+        // Creates a node using null as children and confirms creation
         ASTNode astAnd = ASTNode.createAndNode(null, null);
+        Assert.assertTrue(astAnd.child1 == null);
+        Assert.assertTrue(astAnd.child2 == null);
         // Asserts to confirm type
         Assert.assertTrue(astAnd.isAnd());
         Assert.assertFalse(astAnd.isId());
@@ -19,15 +21,21 @@ public class ASTNodeTest {
         ASTNode astChild = ASTNode.createAndNode(astAnd, astAnd);
         // Gets the name of the node type
         Assert.assertTrue(astChild.getNodeType().equals("AND"));
+        // Ensures a child was produced correctly
+        Assert.assertTrue(astChild.child1.equals(astAnd));
+        Assert.assertTrue(astChild.child2.equals(astAnd));
 
 
 
 	}
 
+    // Tests the functions of the OR node type
     @Test
     public void testOR() {
-        // Creates a node using null as children
+        // Creates a node using null as children and confirms cration
         ASTNode astOr = ASTNode.createOrNode(null, null);
+        Assert.assertTrue(astOr.child1 == null);
+        Assert.assertTrue(astOr.child2 == null);
         // Asserts to confirm type
         Assert.assertTrue(astOr.isOr());
         Assert.assertFalse(astOr.isAnd());
@@ -37,12 +45,17 @@ public class ASTNodeTest {
         ASTNode astChild = ASTNode.createOrNode(astOr, astOr);
         // Gets the name of the node type
         Assert.assertTrue(astChild.getNodeType().equals("OR"));
+        // Ensures a child was produced correctly
+        Assert.assertTrue(astChild.child1.equals(astOr));
+        Assert.assertTrue(astChild.child2.equals(astOr));
     }
 
+    // Tests the functions of the NOT node type
     @Test
     public void testNot() {
-        // Creates a node using null as children
+        // Creates a node using null as children and confirms creation
         ASTNode astNot = ASTNode.createNotNode(null);
+        Assert.assertTrue(astNot.child1 == null);
         // Asserts to confirm type
         Assert.assertTrue(astNot.isNot());
         Assert.assertFalse(astNot.isOr());
@@ -52,13 +65,17 @@ public class ASTNodeTest {
         ASTNode astChild = ASTNode.createNotNode(astNot);
         // Gets the name of the node type
         Assert.assertTrue(astChild.getNodeType().equals("NOT"));
+        // Ensures a child was produced correctly
+        Assert.assertTrue(astChild.child1.equals(astNot));
 
     }
 
+    // Tests the functions of the Id node type
     @Test
     public void testId() {
-        // Creates a node using null as children
+        // Creates a node using null as children and confirms creation
         ASTNode astId = ASTNode.createIdNode(null);
+        Assert.assertTrue(astId.child1 == null);
         // Asserts to confirm type
         Assert.assertTrue(astId.isId());
         Assert.assertFalse(astId.isOr());
@@ -70,7 +87,5 @@ public class ASTNodeTest {
         Assert.assertTrue(astChild.getNodeType().equals("ID"));
         // Gets the name of the node identifier
         Assert.assertTrue(astChild.getId() == "TEST");
-
-
     }
 }
--- src/PA4BoolSat.java
+++ src/PA4BoolSat.java
@@ -10,10 +10,53 @@ import java.util.Set;
 import bool_exp.ASTNode;
 import bool_exp.BoolSatParser;
 
+/*
+ * INSTRUCTIONS
+ * 
+ * this program determines the satisfiability of a given expression
+ * and prints under what conditions it can be satisfied, if at all.
+ * 
+ * The expression input file should contain only a single line, that line should
+ * contain a well-formed Java boolean expression limited to:
+ * the AND operator &&
+ * the OR operator ||
+ * Parenthesis (, )
+ * the NOT operator !
+ * and valid Java identifiers: i.e strings with no spaces that start with
+ * exactly one letter, or $, or _, that is followed by any number of letters,
+ * digits, $ and _ characters.
+ * 
+ * This expression must be on the first line as
+ * ALL OTHER LINES WILL BE IGNORED
+ * 
+ * A debug mode is availabe for this program, input DEBUG on the second command
+ * line and every enumeration of the given variables will be printed along with
+ * whether they evaluated to true or false
+ * 
+ * In their respective cases, the ouput will look like
+ * input: "given expression"
+ * SAT or UNSAT, depending on whether the expression was satisfiable
+ * enumeration1
+ * enumeration3
+ * enumeration5
+ * ...
+ * Or if debug mode is enabled
+ * enumeration1, "truth value"
+ * enumeration2, "truth value"
+ * enumeration3, "truth value"
+ * enumeration4, "truth value"
+ * enumeration5, "truth value"
+ * ...
+ * 
+ * Written by: Nicholas Hernandez
+ * 
+ */
+
 public class PA4BoolSat {
 
+    // Method calls for each part of the program
     public static void main(String[] args) {
-        // Creates the scanner
+        // Creates the scanner from an input file
         Scanner in = openInfile(args[0]);
         // Collects the expression command
         String command = in.nextLine();
@@ -24,15 +67,18 @@ public class PA4BoolSat {
         // Initializes information holding structures
         int[] sofar = new int[leafNodes.size()];
         HashMap<String, Boolean> endMap = new HashMap<String, Boolean>();
-        // Edits endMap with
+        // Edits endMap with enumerations of each variable being true or false
         enumerate(sofar, leafNodes.size(), leafNodes, 0, endMap, root);
+        // Prints the enumerations and whether the expression is satisfiable
         printAll(endMap, args, command);
-
-
     }
+
+    // Returns a scanner for a given file
     public static Scanner openInfile(String filename) {
+        // Initializes file and scanner
         File file = new File(filename);
         Scanner in = null;
+        // error handling for missing file
         try {
             in = new Scanner(file);
         } catch (FileNotFoundException e) {
@@ -42,11 +88,16 @@ public class PA4BoolSat {
         return in;
     }
 
+    // Recusrively returns the variables at leaf nodes in a hashset from a given
+    // ASTNode
     public static Set<String> getAllLeafNodes(ASTNode node) {
         Set<String> leafNodes = new HashSet<String>();
+        // Basecase has been reached and returns the node name
         if (node.getNodeType() == "ID") {
             leafNodes.add(node.getId());
-        }else if(node.getNodeType() == "NOT"){
+            // If the node is of the not variety then it is unable to recurse
+            // down child2
+        } else if (node.getNodeType() == "NOT") {
             leafNodes.addAll(getAllLeafNodes(node.child1));
             // Otherwise recurse down the left and right children
         } else {
@@ -56,62 +107,80 @@ public class PA4BoolSat {
         return leafNodes;
     }
 
+    // Enumerates all combinations of true and false for a given set of
+    // variables
+    // while doing so, enumerate edits a hashmap "endMap" with a string
+    // representation of the enumeration as its key and whether it is
+    // satisfiable as its value
     public static void enumerate(int[] sofar, int size,
             Set<String> leafNodes, int k, HashMap<String, Boolean> endMap,
             ASTNode root) {
+        // Base case is reached
         if (k == size) {
-
+            // Assigns true/false to variables based on enumeration
             HashMap<String, Boolean> bool = assignBool(sofar, leafNodes);
+            // Creates a string representation of the enumeration
             String assignment = assign(sofar, bool);
-            endMap.put(assignment, process(sofar, leafNodes, bool, root));
+            // edits the ending hashmap
+            endMap.put(assignment, process(bool, root));
             return;
         }
+        // All possible local decisions
         for (int i = 0; i < 2; i++) {
-            sofar[k] = i;
+            sofar[k] = i; // Local decision
             enumerate(sofar, size, leafNodes, k + 1, endMap, root);
-            sofar[k] = 0;
+            sofar[k] = 0; // Local decision undone
 
         }
-
-
     }
 
-    public static boolean process(int[] sofar, Set<String> leafNodes,
-            HashMap<String, Boolean> assignBool, ASTNode root) {
+    // Processes a tree recursively and returns whether it is satisfiable
+    public static boolean process(HashMap<String, Boolean> assignBool,
+            ASTNode root) {
+
+        // Returns the and of child1 and child2
         if (root.getNodeType() == "AND") {
-            return process(sofar, leafNodes, assignBool, root.child1)
-                    && process(sofar, leafNodes, assignBool, root.child2);
+            return process(assignBool, root.child1)
+                    && process(assignBool, root.child2);
+            // Returns the or of child1 and child2
         } else if (root.getNodeType() == "OR") {
-            return process(sofar, leafNodes, assignBool, root.child1)
-                    || process(sofar, leafNodes, assignBool, root.child2);
+            return process(assignBool, root.child1)
+                    || process(assignBool, root.child2);
+
+            // returns the opposite of what was processed
         } else if (root.getNodeType() == "NOT") {
-            return !process(sofar, leafNodes, assignBool, root.child1);
+            return !process(assignBool, root.child1);
+
+            // the only node type left is Id whose value is returned
         } else {
             return assignBool.get(root.getId());
         }
-        
-
-
     }
 
+    // Returns a lexicographically sorted(based on keys) string of an
+    // enumeration
     public static String assign(int[] sofar, HashMap<String, Boolean> bool) {
         String str="";
-        int iter = 0;
+        // Sorts the hashmap of keys
         ArrayList<String> list = new ArrayList<String>(bool.keySet());
         Collections.sort(list);
+        // Creates a string representation of the enumeration
         for (String s : list) {
             str = str + s + ": " + bool.get(s) + ", ";
-            iter = iter + 1;
         }
         return str;
     }
 
+    // Returns a hashmap of variables to true or false based on an enumeration
     public static HashMap<String, Boolean> assignBool(int[] sofar,
             Set<String> leafNodes) {
+        // Initializations
         HashMap<String, Boolean> hashMap = new HashMap<String, Boolean>();
         int iter = 0;
         Boolean value = null;
+        // Places each variable to its true or false value in the hashmap
         for (String s : leafNodes) {
+            // Converts 0 or 1 to false or true
             if (sofar[iter] == 0) {
                 value = false;
             } else {
@@ -123,10 +192,14 @@ public class PA4BoolSat {
         return hashMap;
     }
 
+    // Lexicographically prints the possible enumerations
     public static void printAll(HashMap<String, Boolean> endMap,
             String[] args, String command) {
+        // Initializations
         ArrayList<String> list = new ArrayList<String>();
+        // Sat is whether a command was satisfiable
         String sat = "UNSAT";
+        // If debug mode is enabled all enumerations are gathered
         if (args.length == 2) {
             for (String s : endMap.keySet()) {
                 list.add(s + endMap.get(s));
@@ -134,6 +207,7 @@ public class PA4BoolSat {
                     sat = "SAT";
                 }
             }
+            // No debug mode, only true enumerations are gathered
         } else {
             for (String s : endMap.keySet()) {
                 if (endMap.get(s) == true) {
@@ -142,7 +216,10 @@ public class PA4BoolSat {
                 }
             }
         }
+        // Sorts the list of enumerations
         Collections.sort(list);
+        // prints input, satisfiability, and enumerations with/without debug
+        // enabled
         System.out.println("input: " + command);
         System.out.println(sat);
         for (String s : list) {
