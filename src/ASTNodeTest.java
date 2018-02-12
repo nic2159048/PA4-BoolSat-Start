1a595149fc1027b8ccb15d482f0eaae80feb2355
SooperMrNinja <SooperMrNinja@10.137.211.146>
2018-02-11 23:26:27 -0700

    Junit done and Hashset created

--- src/ASTNodeTest.java
+++ src/ASTNodeTest.java
@@ -1,11 +1,75 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import bool_exp.ASTNode;
 
 
 public class ASTNodeTest {
-
+    // Tests the functions of the AND node type
 	@Test
 	public void testAND() {
-        assert 1 == 1;
+        // Creates a node using null as children
+        ASTNode astAnd = ASTNode.createAndNode(null, null);
+        // Asserts to confirm type
+        Assert.assertTrue(astAnd.isAnd());
+        Assert.assertFalse(astAnd.isId());
+        Assert.assertFalse(astAnd.isOr());
+        Assert.assertFalse(astAnd.isNot());
+        // Node created using a node
+        ASTNode astChild = ASTNode.createAndNode(astAnd, astAnd);
+        // Gets the name of the node type
+        Assert.assertTrue(astChild.getNodeType().equals("AND"));
+
+
 	}
 
+    @Test
+    public void testOR() {
+        // Creates a node using null as children
+        ASTNode astOr = ASTNode.createOrNode(null, null);
+        // Asserts to confirm type
+        Assert.assertTrue(astOr.isOr());
+        Assert.assertFalse(astOr.isAnd());
+        Assert.assertFalse(astOr.isId());
+        Assert.assertFalse(astOr.isNot());
+        // Node created using a node
+        ASTNode astChild = ASTNode.createOrNode(astOr, astOr);
+        // Gets the name of the node type
+        Assert.assertTrue(astChild.getNodeType().equals("OR"));
+    }
+
+    @Test
+    public void testNot() {
+        // Creates a node using null as children
+        ASTNode astNot = ASTNode.createNotNode(null);
+        // Asserts to confirm type
+        Assert.assertTrue(astNot.isNot());
+        Assert.assertFalse(astNot.isOr());
+        Assert.assertFalse(astNot.isAnd());
+        Assert.assertFalse(astNot.isId());
+        // Node created using a node
+        ASTNode astChild = ASTNode.createNotNode(astNot);
+        // Gets the name of the node type
+        Assert.assertTrue(astChild.getNodeType().equals("NOT"));
+
+    }
+
+    @Test
+    public void testId() {
+        // Creates a node using null as children
+        ASTNode astId = ASTNode.createIdNode(null);
+        // Asserts to confirm type
+        Assert.assertTrue(astId.isId());
+        Assert.assertFalse(astId.isOr());
+        Assert.assertFalse(astId.isAnd());
+        Assert.assertFalse(astId.isNot());
+        // Node created using a string
+        ASTNode astChild = ASTNode.createIdNode("TEST");
+        // Gets the name of the node type
+        Assert.assertTrue(astChild.getNodeType().equals("ID"));
+        // Gets the name of the node identifier
+        Assert.assertTrue(astChild.getId() == "TEST");
+
+
+    }
 }
--- src/PA4BoolSat.java
+++ src/PA4BoolSat.java
@@ -1,9 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 import bool_exp.ASTNode;
 import bool_exp.BoolSatParser;
 
 public class PA4BoolSat {
 
     public static void main(String[] args) {
+    
+
+        Scanner in = openInfile(args[0]);
+        ASTNode root = BoolSatParser.parse(in.nextLine());
+        Set<ASTNode> leafNodes = getAllLeafNodes(root);
+        System.out.println(getAllLeafNodes(root).size());
+        // System.out.println(root.child1.getNodeType());
+
+
+    }
+    public static Scanner openInfile(String filename) {
+        File file = new File(filename);
+        Scanner in = null;
+        try {
+            in = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: File not found");
+            System.exit(1);
+        }
+        return in;
     }
 
+
+    public static void traverse() {
+
+    }
+
+    public static Set<ASTNode> getAllLeafNodes(ASTNode node) {
+        Set<ASTNode> leafNodes = new HashSet<ASTNode>();
+        if (node.getNodeType() == "ID") {
+            leafNodes.add(node);
+        }else if(node.getNodeType() == "NOT"){
+            leafNodes.addAll(getAllLeafNodes(node.child1));
+            // Otherwise recurse down the left and right children
+        } else {
+                leafNodes.addAll(getAllLeafNodes(node.child1));
+                leafNodes.addAll(getAllLeafNodes(node.child2));
+        }
+        return leafNodes;
+    }
 }
